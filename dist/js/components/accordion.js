8d80d8a6ece743f5bcfe16d816ad2cacd02963cf
leonelnexman <reshevdev@gmail.com>
2024-01-17 10:24:01 +0300

    EasyDb

--- dist/js/components/accordion.js
+++ dist/js/components/accordion.js
@@ -1,52 +1,46 @@
 export class Accordion {
-  constructor() {
-      document.addEventListener('DOMContentLoaded', function () {
-          const questionTops = document.querySelectorAll('.faq__question-top');
-
-          questionTops.forEach(function (questionTop, index) {
-              const content = questionTop.parentNode.querySelector('.faq__question-content');
-              const closeBtn = questionTop.querySelector('.close');
-
-              questionTop.addEventListener('click', function () {
-                  const maxHeight = content.scrollHeight + 'px';
-                  content.style.maxHeight = content.classList.contains('visible') ? 0 : maxHeight;
-                  content.classList.toggle('visible');
-                  closeBtn.classList.toggle('active');
-              });
-
-              closeBtn.addEventListener('click', function (event) {
-                  event.stopPropagation(); // Prevent the click event from propagating to the questionTop
-                  content.style.maxHeight = 0;
-                  content.classList.remove('visible');
-                  closeBtn.classList.remove('active');
-              });
-
-              // Open the first accordion on page load
-              if (index === 0) {
-                  // Trigger a click event on the first questionTop element
-                  const event = new Event('click');
-                  questionTop.dispatchEvent(event);
-              }
+    constructor() {
+      document.addEventListener('DOMContentLoaded', () => {
+        const questionTops = document.querySelectorAll('.faq__question-top');
+  
+        questionTops.forEach((questionTop, index) => {
+          const content = questionTop.parentNode.querySelector('.faq__question-content');
+          const closeBtn = questionTop.querySelector('.close');
+  
+          const toggleContent = () => {
+            const maxHeight = content.scrollHeight + 'px';
+            content.style.maxHeight = content.classList.contains('visible') ? 0 : maxHeight;
+            content.classList.toggle('visible');
+            closeBtn.classList.toggle('active');
+          };
+  
+          questionTop.addEventListener('click', toggleContent);
+          closeBtn.addEventListener('click', (event) => {
+            event.stopPropagation();
+            content.style.maxHeight = 0;
+            content.classList.remove('visible');
+            closeBtn.classList.remove('active');
           });
+  
+          if (index === 0) {
+            const event = new Event('click');
+            questionTop.dispatchEvent(event);
+          }
+        });
+  
+        const itemTop = document.querySelector('.aditions__item-top');
+        const itemContent = document.querySelector('.aditions__item-content');
+        const item = document.querySelector('.aditions__item');
+        const close = document.querySelector('.close');
+  
+        itemTop.addEventListener('click', () => {
+          item.classList.toggle('active');
+          close.classList.toggle('active');
+  
+          const maxHeight = itemContent.scrollHeight + 'px';
+          itemContent.style.maxHeight = item.classList.contains('active') ? maxHeight : '0';
+        });
       });
-
-      document.addEventListener('DOMContentLoaded', function () {
-          const itemTop = document.querySelector('.aditions__item-top');
-          const itemContent = document.querySelector('.aditions__item-content');
-          const item = document.querySelector('.aditions__item');
-          const close = document.querySelector('.close');
-
-          itemTop.addEventListener('click', function () {
-              item.classList.toggle('active');
-              close.classList.toggle('active');
-
-              // Automatically calculate max-height for the element aditions__item-content
-              if (item.classList.contains('active')) {
-                  itemContent.style.maxHeight = itemContent.scrollHeight + 'px';
-              } else {
-                  itemContent.style.maxHeight = '0';
-              }
-          });
-      });
+    }
   }
-}
+  
--- dist/js/components/modal.js
+++ dist/js/components/modal.js
@@ -1,38 +1,33 @@
 export class Modal {
   constructor() {
-    document.addEventListener('DOMContentLoaded', function () {
-      document.body.addEventListener('click', function (event) {
+    document.addEventListener('DOMContentLoaded', () => {
+      const modal = document.getElementById('myModal');
+      const openModalBtn = document.body;
+
+      openModalBtn.addEventListener('click', (event) => {
         const openModalBtn = event.target.closest('.open-modal');
         if (openModalBtn) {
-          document.getElementById('myModal').style.display = 'flex';
+          modal.style.display = 'flex';
         }
       });
 
-      // JavaScript to close the modal when the close button is clicked
-      document.getElementById('closeModalBtn').addEventListener('click', function () {
-        closeModal();
-      });
+      document.getElementById('closeModalBtn').addEventListener('click', () => closeModal());
 
-      // Close the modal if the overlay (outside the modal) is clicked
-      window.addEventListener('click', function (event) {
-        const modal = document.getElementById('myModal');
+      window.addEventListener('click', (event) => {
         if (event.target === modal) {
           closeModal();
         }
       });
 
-      // Event listener for the button with class 'modal__successfully-btn'
-      document.querySelector('.modal__successfully-btn').addEventListener('click', function () {
+      document.querySelector('.modal__successfully-btn').addEventListener('click', () => {
         closeModal();
-        // Remove any added classes from the modal__content element
         const modalContent = document.querySelector('.modal__content');
-        modalContent.classList.remove('successfully', 'error', /* ... */);
+        modalContent.classList.remove('successfully', 'error');
       });
 
       const goTopButton = document.querySelector('.footer__go-top');
 
-      goTopButton.addEventListener('click', function () {
-        // Используем smooth behavior для плавного скроллинга
+      goTopButton.addEventListener('click', () => {
         window.scrollTo({
           top: 0,
           behavior: 'smooth'
@@ -40,8 +35,8 @@ export class Modal {
       });
     });
 
-    function closeModal() {
+    const closeModal = () => {
       document.getElementById('myModal').style.display = '';
-    }
+    };
   }
 }
